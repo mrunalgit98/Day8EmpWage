4ccc93e73dccd6ed9dd3df24c1d1f027fa413c7f
mrunal <mrunalbhoir100@gmail.com>
2022-07-16 10:31:44 +0530

    UC7

--- src/com/empwage/empWage.java
+++ src/com/empwage/empWage.java
@@ -1,39 +1,55 @@
 package com.empwage;
 
-//Calculate Wages till a condition of total working hours of 100 or max days  20 is reached for a month
+import java.util.Scanner;
 
-public class empWage {
-	public static final int IS_PART_TIME=1;
-	public static final int IS_FULL_TIME=2;
+//Refactor the Code to write a Class Method to Compute Employee Wage - Use Class Method and Class Variables
+
+public class empWage{
+	
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=2;
 	public static final int MAX_HRS_IN_MONTH=10;
 	
-	
-	public static void main(String[] args) {
+	public static void employeeWage() {
+		
 		int empHrs=0;
 		int totalEmpHrs=0;
-		int totalEmpWage=0;
 		int totalWorkingDays=0;
-		while (totalEmpWage<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-			
-		totalWorkingDays++;	
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		switch (empCheck) {
+		
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+		
+		int empType=(int)Math.floor(Math.random()*10%2);
+		switch(empType) {
+		
+		case IS_FULL_TIME: 
+			System.out.println("EMPLOYEE IS FULL TIME");
+			empHrs=8;
+			break;
 		case IS_PART_TIME:
-		empHrs=4;
-		break;
-		case IS_FULL_TIME:
-		empHrs=8;
-		break;
-		default:
-			empHrs=0;
+			System.out.println("EMPLOYEE IS PART TIME");
+			empHrs=4;
+			break;
+			
+			default:
+				System.out.println("EMPLOYEE IS ABSENT");
+				empHrs=0;
+			
 		}
 		totalEmpHrs+=empHrs;
-		System.out.println("day:" +totalWorkingDays + " emp hrs " +empHrs);
+		System.out.println("DAYS " +totalWorkingDays + "EMP HRS " + empHrs);
 		}
-	    totalEmpWage=totalEmpHrs* EMP_RATE_PER_HOUR;
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		System.out.println("EMPLOYEE WAGE  " +totalEmpWage);
 		
-		System.out.println("TOTAL EMP WAGE "+ totalEmpWage);	
+		
+	}
+	public static void main(String[] args) {
+		
+		
+		employeeWage();
 	}
 }
+
