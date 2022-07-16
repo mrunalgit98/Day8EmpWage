f2a8db1dba85c1d8197226dd2ab803f671d449ab
mrunal <mrunalbhoir100@gmail.com>
2022-07-16 08:57:25 +0530

    UC2

--- src/com/empwage/empWage.java
+++ src/com/empwage/empWage.java
@@ -1,16 +1,21 @@
 package com.empwage;
 
-//Check Employee is Present or Absent
+//Calculate Daily Employee Wage
 
 public class empWage {
-	
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if (empCheck == IS_FULL_TIME)
-		System.out.println("employee is present");
+		//constant
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		//variable
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if (empCheck==IS_FULL_TIME)
+			empHrs=8;
 		else
-			System.out.println("employee is absent");
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+			System.out.println("emp Wage -- " +empWage);
 	}
-	
 }
